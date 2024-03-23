18a5b2f494554c40e2520bf0094eecc6dc01c4c1
Cem Celebi <cemfcelebi@gmail.com>
2024-03-23 06:14:28 -0400

    Added login signup

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
+    path(route='', view=views.index, name='index'),
+
     # path for about view
     path(route='about/', view=views.about, name='about'),
 
@@ -16,15 +18,14 @@ urlpatterns = [
     path(route='contact/', view=views.contact, name='contact'),
 
     # path for registration
+    path(route='registration/', view=views.registration_request, name='registration'),
 
     # path for login
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
-
-    path(route='', view=views.get_dealerships, name='index'),
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     # path for dealer reviews view
-
-    # path for add a review view
-
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
